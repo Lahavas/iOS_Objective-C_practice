8238bb744c66b6c5b24f254a40fa058836e818db
Lahavas <uv187sn@naver.com>
2017-11-05 01:58:18 +0900

    Set outlet and action

--- Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
+++ Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
@@ -10,6 +10,9 @@
 
 @interface BNRQuizViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation BNRQuizViewController
@@ -23,15 +26,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)showQuestion:(id)sender {
 }
-*/
+
+- (IBAction)showAnswer:(id)sender {
+}
+
 
 @end
