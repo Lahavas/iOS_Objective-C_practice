9bbfc3c8543f2dd2853d09d9a9f62125a76f7d06
Lahavas <uv187sn@naver.com>
2017-11-05 02:17:39 +0900

    Update Action Method

--- Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
+++ Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
@@ -10,17 +10,25 @@
 
 @interface BNRQuizViewController ()
 
+#pragma mark - Property
+
 @property (nonatomic) int currentQuestionIndex;
 
 @property (nonatomic, copy) NSArray *questions;
 @property (nonatomic, copy) NSArray *answers;
+
+#pragma mark - Outlets
 
 @property (weak, nonatomic) IBOutlet UILabel *questionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *answerLabel;
 
 @end
 
+#pragma mark -
+
 @implementation BNRQuizViewController
+
+#pragma mark - Initialization
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil
                          bundle:(NSBundle *)nibBundleOrNil {
@@ -39,20 +47,40 @@
     return self;
 }
 
+#pragma mark - View Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
 
+#pragma mark - Memory Management
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Actions
+
 - (IBAction)showQuestion:(id)sender {
+    _currentQuestionIndex++;
+    
+    if (_currentQuestionIndex == [self.questions count]) {
+        _currentQuestionIndex = 0;
+    }
+    
+    NSString *question = [self.questions objectAtIndex:self.currentQuestionIndex];
+    
+    [self.questionLabel setText:question];
+    
+    [self.answerLabel setText:@"???"];
 }
 
 - (IBAction)showAnswer:(id)sender {
+    NSString *answer = [self.answers objectAtIndex:self.currentQuestionIndex];
+    
+    [self.answerLabel setText:answer];
 }
-
 
 @end
