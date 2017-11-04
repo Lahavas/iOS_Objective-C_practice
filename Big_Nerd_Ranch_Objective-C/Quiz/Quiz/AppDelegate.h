a46de793f902263b6d5d9537148ed72b5a768727
Lahavas <uv187sn@naver.com>
2017-11-05 01:01:48 +0900

    Create Quiz Project

---
+++ Big_Nerd_Ranch_Objective-C/Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Jaeho on 2017. 11. 5..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Big_Nerd_Ranch_Objective-C/Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Jaeho on 2017. 11. 5..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
