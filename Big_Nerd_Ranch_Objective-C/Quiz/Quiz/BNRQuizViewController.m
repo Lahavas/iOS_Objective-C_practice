4f285f7a12daa2d339fbbd4f2c0476755be9ab82
Lahavas <uv187sn@naver.com>
2017-11-05 02:02:14 +0900

    Create Model

--- Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
+++ Big_Nerd_Ranch_Objective-C/Quiz/Quiz/BNRQuizViewController.m
@@ -10,12 +10,34 @@
 
 @interface BNRQuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
 @property (weak, nonatomic) IBOutlet UILabel *questionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *answerLabel;
 
 @end
 
 @implementation BNRQuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
