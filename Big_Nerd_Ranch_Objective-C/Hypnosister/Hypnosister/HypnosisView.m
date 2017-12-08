228a96288da0d63b74496986cb70efba070fa4ad
Lahavas <uv187sn@naver.com>
2017-12-08 18:14:10 +0900

    Update Drawing Process

--- Big_Nerd_Ranch_Objective-C/Hypnosister/Hypnosister/HypnosisView.m
+++ Big_Nerd_Ranch_Objective-C/Hypnosister/Hypnosister/HypnosisView.m
@@ -8,17 +8,28 @@
 
 #import "HypnosisView.h"
 
+@interface HypnosisView()
+
+@property (nonatomic, strong) UIColor *circleColor;
+
+@end
+
 @implementation HypnosisView
+
+#pragma mark - Initialization
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     
     if (self) {
         self.backgroundColor = UIColor.clearColor;
+        self.circleColor = UIColor.lightGrayColor;
     }
     
     return self;
 }
+
+#pragma mark - Drawing View
 
 - (void)drawRect:(CGRect)rect {
     CGRect bounds = self.bounds;
@@ -27,22 +38,23 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    CGFloat radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    CGFloat maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
     
     path.lineWidth = 10;
     
-    [path addArcWithCenter:center
-                    radius:radius
-                startAngle:0.0
-                  endAngle:M_PI * 2.0
-                 clockwise:YES];
-    [UIColor.lightGrayColor setStroke];
+    for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0.0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+        [self.circleColor setStroke];
+    }
     
     [path stroke];
-    
-    
 }
 
 @end
