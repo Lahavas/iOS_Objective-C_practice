1ee39952e4c470d5af22335c239fb8e9b014e57d
Lahavas <uv187sn@naver.com>
2017-12-10 16:07:58 +0900

    Create Project 2. RandomItems

---
+++ Big_Nerd_Ranch_Objective-C/RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Jaeho on 2017. 12. 10..
+//  Copyright © 2017년 yeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
