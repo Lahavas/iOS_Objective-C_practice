0896eec7a66b2d5c32783f854389e4a6debfda5b
Lahavas <uv187sn@naver.com>
2017-12-13 16:42:02 +0900

    Update message into Random Position

--- Big_Nerd_Ranch_Objective-C/HypnoNerd/HypnoNerd/HypnosisViewController.m
+++ Big_Nerd_Ranch_Objective-C/HypnoNerd/HypnoNerd/HypnosisViewController.m
@@ -10,7 +10,7 @@
 
 #import "HypnosisView.h"
 
-@interface HypnosisViewController ()
+@interface HypnosisViewController () <UITextFieldDelegate>
 
 @end
 
@@ -34,7 +34,19 @@
 #pragma mark - View Life Cycle
 
 - (void)loadView {
-    HypnosisView *backgroundView = [[HypnosisView alloc] initWithFrame:CGRectZero];
+    CGRect frame = UIScreen.mainScreen.bounds;
+    HypnosisView *backgroundView = [[HypnosisView alloc] initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    
+    [textField setBorderStyle:UITextBorderStyleRoundedRect];
+    [textField setPlaceholder:@"Hypnotize me"];
+    [textField setReturnKeyType:UIReturnKeyDone];
+    
+    [textField setDelegate:self];
+    
+    [backgroundView addSubview:textField];
     
     self.view = backgroundView;
 }
@@ -43,6 +55,43 @@
     [super viewDidLoad];
     
     NSLog(@"HypnosisViewController loaded its view.");
+}
+
+#pragma mark - Private Methods
+
+- (void)drawHypnoticMessage:(NSString *)message {
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        
+        [messageLabel setBackgroundColor:UIColor.clearColor];
+        [messageLabel setTextColor:UIColor.whiteColor];
+        [messageLabel setText:message];
+        
+        [messageLabel sizeToFit];
+        
+        int width = (int)(CGRectGetWidth(self.view.bounds) - CGRectGetWidth(messageLabel.bounds));
+        int x = arc4random() % width;
+        
+        int height = (int)(CGRectGetHeight(self.view.bounds) - CGRectGetHeight(messageLabel.bounds));
+        int y = arc4random() % height;
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        [messageLabel setFrame:frame];
+        
+        [self.view addSubview:messageLabel];
+    }
+}
+
+#pragma mark - Text Field Delegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self drawHypnoticMessage:textField.text];
+    
+    [textField setText:@""];
+    [textField resignFirstResponder];
+    
+    return YES;
 }
 
 @end
