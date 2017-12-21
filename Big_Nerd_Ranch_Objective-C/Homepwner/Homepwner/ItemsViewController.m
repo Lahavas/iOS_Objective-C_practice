99fcc3e2e211b7a5d900f2a3d0beda1b93003deb
Lahavas <uv187sn@naver.com>
2017-12-21 16:10:56 +0900

    Update Table Header View

--- Big_Nerd_Ranch_Objective-C/Homepwner/Homepwner/ItemsViewController.m
+++ Big_Nerd_Ranch_Objective-C/Homepwner/Homepwner/ItemsViewController.m
@@ -12,6 +12,10 @@
 
 @interface ItemsViewController ()
 
+#pragma mark - Private IBOutlet Property
+
+@property (strong, nonatomic) IBOutlet UIView *headerView;
+
 @end
 
 @implementation ItemsViewController
@@ -41,6 +45,41 @@
     
     [self.tableView registerClass:[UITableViewCell class]
            forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+}
+
+#pragma mark - Accessor Methods
+
+- (UIView *)headerView {
+    if (!_headerView) {
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView"
+                                      owner:self
+                                    options:nil];
+    }
+    
+    return _headerView;
+}
+
+#pragma mark - IBAction
+
+- (IBAction)addNewItem:(id)sender {
+    
+}
+
+- (IBAction)toggleEditingMode:(id)sender {
+    if (self.isEditing) {
+        [sender setTitle:@"Edit"
+                forState:UIControlStateNormal];
+        [self setEditing:NO
+                animated:YES];
+    } else {
+        [sender setTitle:@"Done"
+                forState:UIControlStateNormal];
+        [self setEditing:YES
+                animated:YES];
+    }
 }
 
 #pragma mark - Table View Data Source
